0b3c8ad98e5f231b4ee3af282f2b1ee33aac0369
Musfiqur Rahman <mushfiqur15-10872@diu.edu.bd>
2023-10-11 13:26:09 +0600

    Data retrived successfully

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 }
 
 android {
+
+    buildFeatures{
+        viewBinding = true
+    }
     namespace = "org.freedu.studentmanagementclient"
     compileSdk = 33
 
@@ -46,4 +50,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
